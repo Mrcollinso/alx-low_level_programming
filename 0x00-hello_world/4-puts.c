5152c3b25039ed93b626657c6d611adb4c1de6b2
mrcollinso <collinsonyebuchi111@gmail.com>
2022-12-09 08:19:59 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        puts("\" programming is like building a multilingua puzzle");
+	return (0);
+
+}	
