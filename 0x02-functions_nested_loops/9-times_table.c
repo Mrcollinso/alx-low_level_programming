0181147cf5492b862710c401ae2af854806aa89b
mrcollinso <collinsonyebuchi111@gmail.com>
2022-12-19 02:11:20 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,8 +7,9 @@ void times_table(void)
 {
 	int i, j, k;
 
+	for (i = 0; i < 10; i++)
 	{
-		for (i = 0; i < 10; i++)
+		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
 			if (j == 0)
@@ -33,4 +34,3 @@ void times_table(void)
 		_putchar('\n');
 	}
 }
-	
